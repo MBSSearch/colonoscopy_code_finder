797b8b4293de7aef6d53f384a03c22c5826a7cb5
Giovanni Lodi <giovanni.lodi42@gmail.com>
2019-10-06 21:28:35 +1100

    Implement request from S3, no JSON decoding

--- src/Main.elm
+++ src/Main.elm
@@ -2,36 +2,87 @@ module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
 import Html exposing (Html, div, text)
+import Http
+import Json.Decode exposing (Decoder, field, string)
 
 
 main : Program () Model Msg
 main =
     Browser.element
-        { init = \_ -> ( init, Cmd.none )
+        { init = init
         , view = view
         , update = update
         , subscriptions = \_ -> Sub.none
         }
 
 
-type alias Model =
-    {}
+type Model
+    = Loading
+    | Failure Http.Error
+    | Success String
 
 
-init : Model
-init =
-    {}
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Loading, getDecisionTree )
 
 
 type Msg
-    = NoOp
+    = GotDecisionTree (Result Http.Error String)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update _ model =
-    ( model, Cmd.none )
+update msg _ =
+    case msg of
+        GotDecisionTree result ->
+            case result of
+                Ok value ->
+                    ( Success value, Cmd.none )
+
+                Err error ->
+                    ( Failure error, Cmd.none )
 
 
 view : Model -> Html Msg
-view _ =
-    div [] [ text "Hello world" ]
+view model =
+    case model of
+        Loading ->
+            div [] [ text "Loading..." ]
+
+        Failure error ->
+            div [] [ text <| "There's been an error: " ++ toString error ]
+
+        Success value ->
+            div [] [ text <| "Loaded. I haven't figured out how to decode the tree yet, but the first question is: " ++ value ]
+
+
+getDecisionTree : Cmd Msg
+getDecisionTree =
+    Http.get
+        { url = "https://s3-ap-southeast-2.amazonaws.com/static.mbssearch.com/colonoscopy_decision_tree.json"
+        , expect = Http.expectJson GotDecisionTree decisionTreeDecoder
+        }
+
+
+decisionTreeDecoder : Decoder String
+decisionTreeDecoder =
+    field "root" (field "text" string)
+
+
+toString : Http.Error -> String
+toString error =
+    case error of
+        Http.BadUrl string ->
+            "bad url" ++ string
+
+        Http.Timeout ->
+            "timeout"
+
+        Http.NetworkError ->
+            "network error"
+
+        Http.BadStatus code ->
+            "bad status" ++ String.fromInt code
+
+        Http.BadBody string ->
+            "bad body" ++ string
